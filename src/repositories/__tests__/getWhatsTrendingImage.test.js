929235819352ff6f987af1541754e37b647831c6
Jorge Alvarez Poza <jalvarez@admins-MacBook-Pro.local>
2020-04-18 18:44:56 +0200

    getWhatsTrendingImage adapts the service response

--- src/repositories/__tests__/getWhatsTrendingImage.test.js
+++ src/repositories/__tests__/getWhatsTrendingImage.test.js
@@ -1,13 +1,21 @@
 import { getWhatsTrendingImage } from '../getWhatsTrendingImage';
 
-it('returns the what\'s trending image from the server', () => {
-	const serviceResponse = 'any service response';
+it('returns the what\'s trending image from the server', (done) => {
+	const trendingImageUrl = 'any trending image url';
+	const serviceResponse = {
+		text: jest.fn()
+	};
+	const servideResponsePromise = Promise.resolve(serviceResponse);
 
 	jest.spyOn(window, 'fetch');
-	fetch.mockReturnValueOnce(serviceResponse);
+	fetch.mockReturnValueOnce(servideResponsePromise);
+	serviceResponse.text.mockReturnValueOnce(trendingImageUrl);
 
-	const response = getWhatsTrendingImage();
+	const promiseResult = getWhatsTrendingImage();
 
 	expect(fetch).toHaveBeenNthCalledWith(1, 'http://localhost:7000');
-	expect(response).toBe(serviceResponse);
+	promiseResult.then((result) => {
+		expect(result).toBe(trendingImageUrl);
+		done();
+	});
 });
