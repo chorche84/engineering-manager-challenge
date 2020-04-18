60dbe2b10525dcc66b3dc0df120e70f925d9eb48
Jorge Alvarez Poza <jalvarez@admins-MacBook-Pro.local>
2020-04-18 13:56:12 +0200

    Trending component is defined as a class

--- src/components/Landing/Trending/Trending.js
+++ src/components/Landing/Trending/Trending.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import Promo from "../Promo";
 
-const Trending = () => (
-  <Promo title="Whats trending " size={"xlarge"}>
-    <picture>
-      <img src={""} alt="What's trending" />
-    </picture>
-  </Promo>
-);
+class Trending extends Component {
+  render () {
+    return (
+      <Promo title="Whats trending " size={"xlarge"}>
+        <picture>
+          <img src={""} alt="What's trending" />
+        </picture>
+      </Promo>
+    );
+  }
+};
 
 export default Trending;
