9625c6cbe865a7deef7e23a3580bb9ba08fa9767
Jorge Alvarez Poza <jalvarez@admins-MacBook-Pro.local>
2020-08-07 18:58:45 +0200

    layout component as been deleted and integrated into Landing component, in order to remove an unneded layer of props propagation

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { MiniModeProvider } from "../context/MiniMode";
-import Layout from "../components/Layout";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Hero from "../components/Landing/Hero";
 import WeAre from "../components/Landing/WeAre";
 import Trending from "../components/Landing/Trending";
@@ -115,29 +116,29 @@ const data = {
 const Landing = () => {
   return (
     <MiniModeProvider>
-      <Layout header={data.header} stickyBanner={data.stickyBanner}>
-        <Hero
-          background={"/images/starzplay_covers"}
-          heroTitle="Imagine the best entertainment all in one place"
-          icon={"languageGlobe"}
-          size={"largest"}
-        />
-        <WeAre />
-        <Trending />
-        <Promo
-          background={"/images/promo1"}
-          backgroundLocale={true}
-          text="Turn your Smart TV into <strong>Brillian TV</strong>"
-          direction={"end"}
-        />
-        <Promo
-          background={"/images/promo2"}
-          backgroundLocale={true}
-          text="Get <strong>thousands</strong> of titles in your pocket"
-          direction="start"
-        />
-        <Tabs layout={data.tabsLayout} />
-      </Layout>
+      <Header label={data.header.label} list={data.header.list} dropDown={data.header.dropDown} stickyBanner={data.stickyBanner}/>
+      <Hero
+        background={"/images/starzplay_covers"}
+        heroTitle="Imagine the best entertainment all in one place"
+        icon={"languageGlobe"}
+        size={"largest"}
+      />
+      <WeAre />
+      <Trending />
+      <Promo
+        background={"/images/promo1"}
+        backgroundLocale={true}
+        text="Turn your Smart TV into <strong>Brillian TV</strong>"
+        direction={"end"}
+      />
+      <Promo
+        background={"/images/promo2"}
+        backgroundLocale={true}
+        text="Get <strong>thousands</strong> of titles in your pocket"
+        direction="start"
+      />
+      <Tabs layout={data.tabsLayout} />
+      <Footer/>
       <StickyBanner heading={data.stickyBanner.heading} buttons={data.stickyBanner.buttons}/>
     </MiniModeProvider>
   );
